13d6f0d3389581f1a722b52c89c5f0ef09e014a3
takumi314 <isospin_symmetry@me.com>
2018-06-28 20:10:34 +0900

    first commit

---
+++ MKBuilder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MKBuilder
+//
+//  Created by NishiokaKohei on 2018/06/28.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
