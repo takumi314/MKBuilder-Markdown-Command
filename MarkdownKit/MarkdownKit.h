cfa40f83c24c890c80daca2c18ab8e3bfdbe89f4
takumi314 <isospin_symmetry@me.com>
2018-06-28 20:14:10 +0900

    add framework target

---
+++ MarkdownKit/MarkdownKit.h
@@ -0,0 +1,19 @@
+//
+//  MarkdownKit.h
+//  MarkdownKit
+//
+//  Created by NishiokaKohei on 2018/06/28.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MarkdownKit.
+FOUNDATION_EXPORT double MarkdownKitVersionNumber;
+
+//! Project version string for MarkdownKit.
+FOUNDATION_EXPORT const unsigned char MarkdownKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkdownKit/PublicHeader.h>
+
+
